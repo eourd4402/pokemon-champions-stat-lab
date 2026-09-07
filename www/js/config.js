8bd71fa352bfe1843c231b715e2c21e2a9f78f9f
eourd4402 <eourd4402@gmail.com>
2026-09-07 23:30:06 +0900

    Add files via upload

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 /* 이 파일의 dataBaseUrl 한 곳만 자신의 데이터 저장소 주소로 바꾸세요. */
 window.APP_CONFIG = Object.freeze({
   appVersion: '19.0',
-  dataBaseUrl: 'https://raw.githubusercontent.com/eorud4402/pokemon-champions-data/main',
+  dataBaseUrl: 'https://raw.githubusercontent.com/eourd4402/pokemon-champions-data/main',
   dataManifest: 'manifest.json'
 });
